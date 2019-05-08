f400e865b73df799fcdad242083f785fbbec4688
unknown <bishoymelekwadie@gmail.com>
2019-05-08 14:53:16 +0200

    better performance for big integers

--- lesson2_oddOccurancy.js
+++ lesson2_oddOccurancy.js
@@ -1,12 +1,17 @@
 /*
-    v1
-    Score: 55%
-    Performance tests for big arrays failed
+    v2
+    Score: 100%
+    This one, is expecting the array to has only one odd integer
+    It will sort the array elements,
+    and will loop for half the length of the Array (Sorted in pairs already)
+    and return the odd one
 */
 
 function solution(A) {
-    for (var i = 0; i < A.length; i++) {
-        var found = A.filter((item, index) => A[i] === item);
-        if (found.length === 1) return A[i]
+    var arrSorted = A.sort(function (a, b) {
+        return (a - b);
+    });
+    for (var i = 0; i < arrSorted.length; i += 2) {
+        if ((arrSorted[i] !== arrSorted[i + 1]) || i + 1 == arrSorted.length) return arrSorted[i];
     }
 }
